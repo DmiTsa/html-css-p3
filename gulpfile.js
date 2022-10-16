64a2191be0b9ff805dc7932d44a1fc2dc633d0ca
DmiTsa <you@example.com>
2022-10-16 23:08:12 +0300

    h

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+const browserSync = require('browser-sync');
+const { src, dest, watch } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const rename = require('gulp-rename');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+
+function runServer() {
+  browserSync.init({
+    server: {
+      baseDir: 'src',
+    },
+  });
+}
+
+function sassToCss() {
+  return src('src/sass/*.+(scss|sass)')
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(
+      rename({
+        prefix: '',
+        suffix: '.min',
+      })
+    )
+    .pipe(autoprefixer())
+    .pipe(cleanCSS({ compatibility: 'ie8' }))
+    .pipe(dest('src/css'))
+    .pipe(browserSync.stream());
+}
+
+function watchChanges() {
+  watch('src/sass/*.+(scss|sass)', sassToCss);
+  watch('src/*.html').on('change', browserSync.reload);
+}
+
+function defaultTask() {
+  runServer();
+  sassToCss();
+  watchChanges();
+}
+
+exports.default = defaultTask;
